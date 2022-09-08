a832ece4ba752a233a8be68796ea70c8f6c63203
Developer Junaid <qjunaid623@gmail.com>
2022-09-08 22:56:14 +0500

    added sanity token

--- pages/index.js
+++ pages/index.js
@@ -64,6 +64,7 @@ const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: "2022-09-08",
+  token: process.env.NEXT_PUBLIC_SANITY_AUTH_TOKEN,
   useCdn: false,
 })
 
