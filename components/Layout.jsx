0f86e504709cdfb1f71832a62906ddcc89851fd2
Developer Junaid <qjunaid623@gmail.com>
2022-09-14 11:08:08 +0500

    changed a text

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -253,7 +253,7 @@ export const Layout = ({ children }) => {
                                 <ul role="list" className="space-y-4 border-b border-gray-200 pb-6 text-sm font-medium text-gray-900">
                                     {subCategories.map((category) => (
                                         <li key={category.name}>
-                                            <span className='cursor-pointer' onClick={() => setNewProducts(category.name)}>{category.name}</span>
+                                            <span className='cursor-pointer' onClick={() => setNewProducts(category.name)}>{category.label}</span>
                                         </li>
                                     ))}
                                 </ul>
