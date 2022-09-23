a7690d7887698b86b83afbb26f616ccef1ef8459
Developer Junaid <qjunaid623@gmail.com>
2022-09-23 12:36:13 +0500

    navigate on card click

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 
-// Next
-import Link from 'next/link'
+// Router
+import { useRouter } from 'next/router'
 
-export const ProductCard = ({ imageSrc, imageAlt, name, price, reference }) => (
-    <div className="group">
-        <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-            <Link href={`product/${reference}`}>
+export const ProductCard = ({ imageSrc, imageAlt, name, price, reference }) => {
+    const router = useRouter()
+    return (
+        <div onClick={() => { router.push(`product/${reference}`) }} className="group cursor-pointer">
+            <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
                 <img
                     src={imageSrc}
                     alt={imageAlt}
-                    className="h-full cursor-pointer w-full object-cover object-center group-hover:opacity-75"
-                /></Link>
+                    className="h-full w-full object-cover object-center group-hover:opacity-75"
+                />
+            </div>
+            <h3 className="mt-4 text-sm text-gray-700">{name}</h3>
+            <p className="mt-1 text-lg font-medium text-gray-900">${price}</p>
         </div>
-        <h3 className="mt-4 text-sm text-gray-700">{name}</h3>
-        <p className="mt-1 text-lg font-medium text-gray-900">${price}</p>
-    </div>
-)
+    )
+}
